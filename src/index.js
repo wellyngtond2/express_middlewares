d155c520c17aeeb5132ca18c04b2043ac5345283
siscomex <programevc@gmail.com>
2021-03-13 03:08:52 -0300

    midlewares

---
+++ src/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+function logRequest(request,response,next){
+    const { method, url} = request;
+
+    const logLabel = `[${method.toUpperCase()} ${url}]`;
+
+    console.log(logLabel);
+
+    return next();
+}
+
+function verifyId(request,response,next){
+    const {id}= request.params;
+
+    if(!isUuid(id)){
+        return response.status(400).send({error:'Invalid Id'});
+    }
+
+    return next();
+}
+
+app.use(logRequest);
+app.use('/projects/:id',verifyId);
+
+app.get('/projects', (request, response) => {
+
+    const {title} = request.query;
+
+    const results = title ? projects.filter(proj => proj.title.includes(title)) : projects;
+
+    return response.json(results);
+});
+
+app.post('/projects', (request, response) => {
+    const {title, owner} = request.body;
+
+    const project = {id: uuid(), title, owner};
+
+    projects.push(project);
+
+    return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+    const {id} = request.params;
+    const {title, owner} = request.body;
+
+    const projectIndex = projects.findIndex(proj => proj.id === id);
+
+    if(projectIndex < 0)
+    {
+        return response.status(400).send('project not found');
+    }
+
+    const project = { id, title, owner};
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    const {id} = request.params;
+
+    const projectIndex = projects.findIndex(proj => proj.id === id);
+
+    if(projectIndex < 0)
+    {
+        return response.status(400).send('project not found');
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
+});
+
+app.listen(3333, ()=>{
+    console.log('api-started!');
+});
+
